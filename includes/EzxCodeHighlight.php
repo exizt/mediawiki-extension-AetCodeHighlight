d65c69bfadbfa4d766a3b38fef12088742f634ee
exizt <e2xist@gmail.com>
2022-10-17 15:01:17 +0900

    minor

--- includes/EzxCodeHighlight.php
+++ includes/EzxCodeHighlight.php
@@ -110,8 +110,8 @@ class EzxCodeHighlight {
 		/*
 		* 설정 기본값
 		* 
-		* ClientId : 애드센스 id key 값. (예: ca-pub-xxxxxxxxx)
-		* SlotIdContentTop : 콘텐츠 상단에 표시할 애드센스 광고 단위 아이디 (예: xxxxxxx)
+		* type : 'highlightjs', 'prismjs'
+		* theme : 테마
 		* SlotIdContentBottom : 콘텐츠 히단에 표시할 애드센스 광고 단위 아이디 (예: xxxxxxx)
 		* AnonOnly : '비회원'만 애드센스 노출하기.
 		* DisallowedIPs : 애드센스를 보여주지 않을 IP 목록.
